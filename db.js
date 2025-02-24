8499f9e002ef3e7d4ce9cf186d550ae0228e707d
aya-asylbek <aichurek0990@gmail.com>
2025-02-24 13:40:05 -0800

    Add database connection file

---
+++ db.js
@@ -0,0 +1,10 @@
+import { Pool } from 'pg';
+
+const pool = new Pool({
+    user: 'tpl522_13',  // Replace with your PostgreSQL username
+    host: '/tmp',
+    database: 'books',
+    port: 5432,
+});
+
+export default pool;
