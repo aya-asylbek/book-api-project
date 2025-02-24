50901a820872cf742b6dfebc71edefda2442d031
aya-asylbek <aichurek0990@gmail.com>
2025-02-24 10:39:06 -0800

    Able to run locally get all books and get by id,checked through Postman ,got success response

---
+++ books.js
@@ -0,0 +1,33 @@
+export const BOOKS = [
+    {
+      id: 1,
+      title: "Harry Potter and the Sorcerer's Stone",
+      author: "J.K. Rowling",
+      format: "Paperback"
+    },
+    {
+      id:2,
+      title: "Harry Potter and the Chamber of Secrets",
+      author: "J.K. Rowling",
+      format: "Paperback"
+    },
+    {
+      id:3,
+      title: "Harry Potter and the Prisoner of Azkaban",
+      author: "J.K. Rowling",
+      format: "Paperback"
+    },
+    {
+      id:4,
+      title: "Harry Potter and the Goblet of Fire",
+      author: "J.K. Rowling",
+      format: "Hardcover"
+    },
+    {
+      id:5,
+      title: "Harry Potter and the Order of the Phoenix",
+      author: "J.K. Rowling",
+      format: "Hardcover"
+    },
+  ];
+
---
+++ index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import cors from 'cors';
+import {BOOKS} from './books.js';
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+
+app.use(express.json());
+
+
+// Route to get all books
+app.get('/books', (req, res) => {
+  res.json(BOOKS);  // Send imported data as JSON response
+});
+
+
+app.get('/books/:id', (req, res) => {
+  const bookId = parseInt(req.params.id, 10); // Make sure the book ID is converted to a number
+  const book = BOOKS.find(b => b.id === bookId); // Find the book by ID
+  
+  if (book) {
+    res.json(book);  // Return the book as a JSON response
+  } else {
+    res.status(404).json({ message: 'Book not found' });  // Error if the book is not found
+  }
+});
+
+  //3. Right code for post (to create new book and check on postman)
+
+//4. put (update all credentials=>>>all info)
+
+// Delete (to delete one book for now )
+
+
+app.listen(PORT, () => {
+  console.log('Server running on port 3000');
+});
